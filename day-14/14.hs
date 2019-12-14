6a5df6d318af5b7c8cd951eaa4ce3b709d69b44a
mrlukasbos <mrlukasbos@gmail.com>
2019-12-14 22:14:19 +0100

    some cleaning up for day 14

--- day-14/14.hs
+++ day-14/14.hs
@@ -1,10 +1,7 @@
 import Data.List
 import qualified Data.List.Split as Split
-import Data.Ord
 import Data.Maybe
 import qualified Data.Map.Strict as Map
-import Debug.Trace
-import Data.Char (isSpace)
 
 data Conversion = Conversion {
     ingredients :: [(String, Int)],
@@ -14,9 +11,7 @@ data Conversion = Conversion {
 day_14b :: IO ()
 day_14b = do
     text <- readFile "input.txt"
-    let split_1 = map (map (Split.splitOn ", ")) (map (Split.splitOn " => ") (lines text))
-    let split_2 = map (map item_to_tup) (map (concat) split_1)
-    let conversions = map to_conversion split_2
+    let conversions = lines_to_conversions (lines text)
     print $ brute_force conversions 0 1000000000000 -- calc conversions (Map.fromList (ingredients fuel_conv)) Map.empty
 
 brute_force conversions attempt factor
@@ -30,12 +25,15 @@ brute_force conversions attempt factor
 day_14a :: IO ()
 day_14a = do
     text <- readFile "input.txt"
-    let split_1 = map (map (Split.splitOn ", ")) (map (Split.splitOn " => ") (lines text))
-    let split_2 = map (map item_to_tup) (map (concat) split_1)
-    let conversions = map to_conversion split_2
+    let conversions = lines_to_conversions (lines text)
     let fuel_conv = get_conversion_by_result_str "FUEL" conversions
     print $ calc conversions (Map.fromList (ingredients (fromJust fuel_conv))) Map.empty
     
+lines_to_conversions :: [String] -> [Conversion]
+lines_to_conversions lns = let 
+    split_1 = map (map (Split.splitOn ", ")) (map (Split.splitOn " => ") lns)
+    split_2 = map (map item_to_tup) (map (concat) split_1)
+    in map to_conversion split_2
 
 to_conversion :: [(String, Int)] -> Conversion
 to_conversion items = Conversion {
@@ -43,7 +41,7 @@ to_conversion items = Conversion {
     result = last items 
 }
 
-item_to_tup item = (trim (s !! 1), read (s !! 0) :: Int) where s = Split.splitOn " " item
+item_to_tup item = (s !! 1, read (s !! 0) :: Int) where s = Split.splitOn " " item
 
 -- Get the conversion for a result
 get_conversion_by_result_str :: String -> [Conversion] -> Maybe Conversion
@@ -76,27 +74,18 @@ scale_conversion conversion factor = conversion {
     result = scale_item factor (result conversion)
 }
 
--- scales 
 scale_item :: Int -> (String, Int) -> (String, Int)
 scale_item factor (a,b) = (a, b * factor) 
 
 calc :: [Conversion] -> Map.Map String Int -> Map.Map String Int -> Int
-calc all_conversions requested_ingredients leftovers = let
-    
-    filtered_requests = Map.filterWithKey (\k _ -> k /= "ORE" ) requested_ingredients -- get all entries that are not ore
-    ore_requests = Map.filterWithKey (\k _ -> k == "ORE" ) requested_ingredients -- get all entries that are ore
-
-    filtered_requests_list = Map.toList filtered_requests
-    ore_requests_list = Map.toList ore_requests
-
-    new_status = check_ingredient (head filtered_requests_list) leftovers all_conversions
-    new_requested_ingredients = Map.fromListWith (+) ((snd new_status) ++ (tail filtered_requests_list) ++ (ore_requests_list)) -- the ore needs to be put back in
-    new_leftovers = (fst new_status)
-
-    exec 
-        | filtered_requests_list == [] = sum (map snd ore_requests_list) 
-        | otherwise = {-trace ("requested: " ++ show new_requested_ingredients ++ "-------  leftovers: " ++ (show leftovers)) $ -} calc all_conversions new_requested_ingredients new_leftovers
-    in exec
+calc all_conversions requested_ingredients leftovers
+    | filtered_requests_list == [] = sum (map snd ore_requests_list) 
+    | otherwise = {-trace ("requested: " ++ show new_requested_ingredients ++ "-------  leftovers: " ++ (show leftovers)) $ -} calc all_conversions new_requested_ingredients (fst new_status)
+        where
+            filtered_requests_list = Map.toList $ Map.filterWithKey (\k _ -> k /= "ORE" ) requested_ingredients -- get all entries that are not ore
+            ore_requests_list = Map.toList $ Map.filterWithKey (\k _ -> k == "ORE" ) requested_ingredients -- get all entries that are ore
+            new_status = check_ingredient (head filtered_requests_list) leftovers all_conversions
+            new_requested_ingredients = Map.fromListWith (+) ((snd new_status) ++ (tail filtered_requests_list) ++ (ore_requests_list)) -- the ore needs to be put back in
 
 check_ingredient :: (String, Int) -> Map.Map String Int -> [Conversion] -> (Map.Map String Int, [(String, Int)])
 check_ingredient ingredient leftovers conversions
@@ -108,6 +97,3 @@ check_ingredient ingredient leftovers conversions
             basic_conversion_to_get_ingredient = fromJust $ get_conversion_by_result_str (fst ingredient) conversions
             conversion_to_get_ingredient = get_requested_conversion basic_conversion_to_get_ingredient 1 ((snd ingredient) - leftovers_for_key)
             new_leftover_value = leftovers_for_key + (snd (result conversion_to_get_ingredient)) - (snd ingredient)
-
-trim :: String -> String
-trim = f . f where f = reverse . dropWhile isSpace
